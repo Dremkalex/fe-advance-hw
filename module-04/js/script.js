efdaf917677c9c3812a3157465640268c6074dd0
Mashchenko Oleksandra <dremka@hotmail.com>
2018-02-22 17:54:01 +0200

    row number checking added

--- module-04/js/script.js
+++ module-04/js/script.js
@@ -1,36 +1,46 @@
 const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
-const keyboard = addKeyboardLayout(alphabet);
-console.log(keyboard);
 
-let row = prompt('Введите номер строки, где 0 - верхняя, 1 - средняя, 2 - нижняя');
-let letter = getRandCharInRow(row);
-if (letter) {
-  console.log(`Случайная буква строки ${row}: ${letter} `);
-} else {
-  console.log('Функция не может быть выполнена из-за некорректных данных');
-}
-
-console.log(`Случайная буква алфавита: `, getRandCharInAlph());
-
+//функция, получающая строку-алфавит и возвращающая массив-массивов клавиатурных строк
 function addKeyboardLayout(alphabet) {
   const arr = [alphabet.split("").slice(0, 12), alphabet.split("").slice(12, 23), alphabet.split("").slice(23)];
   return arr;
 }
 
+const keyboard = addKeyboardLayout(alphabet);
+console.log(keyboard);
+
+//функция, принимающая номер строки клавиатуры и возвращающая случайную букву из этой строки
 function getRandCharInRow(row) {
-  // let letters = keyboard[row][Math.floor(Math.random() * keyboard[row].length)];
-  let letters;
-  switch (row) {
-    case '0':
-    case '1':
-    case '2': letters = keyboard[row][Math.floor(Math.random() * (keyboard[row].length))]; break;
-    default: console.log('Некорректный номер строки');
-  }
-  return letters;
+  return keyboard[row][Math.floor(Math.random() * keyboard[row].length)];
 }
 
-function getRandCharInAlph() {
-  let rows = Math.floor(Math.random() * keyboard.length);
-  let letters = keyboard[rows][Math.floor(Math.random() * (keyboard[rows].length))];
-  return letters;
+let row = prompt(`Введите номер, где: \'0\' - верхняя строка, \'1\' - средняя строка, \'2\' - нижняя строка!`);
+let answer = -1;
+while (row !== '0' && row !== '1' && row !== '2' || row === null || row === '') {
+  if (row === null) {
+    answer = confirm('Вы отменили ввод! Хотите продолжить?');
+    if (answer === false) {break;}
+  }
+  else if (row === '') {
+    alert('Вы не ввели данные!');
+  }
+  else {
+    alert('Вы ввели некорректный номер строки!');
+  }
+row = prompt(`Введите номер, где: \'0\' - верхняя строка, \'1\' - средняя строка, \'2\' - нижняя строка!`);
 }
+
+if (answer === -1 || answer === true) {
+  const letter = getRandCharInRow(row);
+  console.log(`Случайная буква строки ${row}: ${letter}`);
+}
+else {
+  alert('Вы отменили выполнение функции поиска буквы по строке!')
+}
+
+//функция, возвращающая случайную букву из всего алфавита.
+function getRandCharInAlph() {
+  const rows = Math.floor(Math.random() * keyboard.length);
+  return keyboard[rows][Math.floor(Math.random() * (keyboard[rows].length))];
+}
+console.log('Случайная буква алфавита: ', getRandCharInAlph());
